da6f409af38d11a250584742a4d687fba4b5bd45
Ben Caneba <bcaneba@gmail.com>
2015-07-26 10:36:27 +0800

    added current_city

---
+++ db/migrate/20150726022029_add_city.rb
@@ -0,0 +1,5 @@
+class AddCity < ActiveRecord::Migration
+  def change
+  	add_column :users, :current_city, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223223243) do
+ActiveRecord::Schema.define(version: 20150726022029) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150223223243) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "current_city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
