e2a212500c0cc5d6f60be0758805d37f44fc59ad
Ben Caneba <bcaneba@gmail.com>
2015-10-20 22:06:47 -0500

    gemfile

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -18,7 +18,6 @@ class ListingsController < ApplicationController
 		@listing = Listing.search(params)
 	end
 
-
 	private
 	def listing_params
 		# params require our listing table, only permit ... parameters to be created 
